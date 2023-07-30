3e064da353200c69621c0630a9719d79fc59b19e
Joynal Abedin <joynal.9933@gmail.com>
2023-07-30 14:39:53 +0600

    1

---
+++ zzz-testform/instantio/admin/tf-options/classes/Ins_ChangeLog.php
@@ -0,0 +1,76 @@
+<?php 
+
+/**
+ *  Instantio Changelog Data.
+ *
+ * @author M Hemel Hasan
+ * @since 3.0
+ * 
+ * @Data Array: Improvement, New, Fix, Optimization, Compatibility!
+ */
+
+$change = [
+    // Update Start
+    'v4' => [
+        'date'      => '23 July 2023',
+        'version'   => 'Version 3.0.3',
+        'changelog' => [
+            'Fix'   =>   [
+                'Customer login collapse issues (Pro)',
+                'Cart shipping price updates in real-time',
+            ],
+        
+            'Improvement'   =>   [
+                'Cart functionality',
+            ],
+        ]
+    ],
+    // Update End
+    
+    // Update Start
+    'v3' => [
+        'date'      => '09 July 2023',
+        'version'   => 'Version 3.0.2',
+        'changelog' => [
+            'Fix'   =>   [
+                'Undefined issue',
+                'Card issues fix',
+                'Card items title issues fix',
+                'Guest checkout issues fixed( Pro )',
+                'Allow user to login during checkout fixed( Pro )',
+            ],
+            
+            'New'   =>  [
+                'New Payment Added (WooCommerce Square)',
+            ],
+
+            'Improvement'   =>   [
+                'Mobile Card items title',
+            ],
+        ]
+    ],
+    // Update End
+    
+    // Update  start
+    'v2' => [
+        'date'      => '24 June 2023',
+        'version'   => 'Version 3.0.1',
+        'changelog' => [
+            'Fix'   =>   [
+                'Undefined issue',
+                'Upsell & Cross Sell issue ( Pro )',
+                'Variation product issue in Cartfly',
+                'Column break Issue fixed( Pro )',
+            ], 
+            'New'   =>   [
+                'Language File Added', 
+            ],
+        ]
+    ],
+    // Update End
+    
+]
+
+
+
+?>
